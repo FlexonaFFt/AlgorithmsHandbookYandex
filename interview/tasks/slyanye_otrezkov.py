07c71530fd3d591415f0f13a3213b5bef17b4610
FlexonaFFt <flexonafft@bk.ru>
2026-01-05 18:14:32 +0300

    --load interview slyanye otrezkov

---
+++ interview/tasks/slyanye_otrezkov.py
@@ -0,0 +1,31 @@
+'''
+Слияние отрезков:
+
+Вход: [1, 3] [100, 200] [2, 4]
+Выход: [1, 4] [100, 200]
+'''
+
+class Solution:
+    def main(self, ranges: list[list[int]]) -> list[list[int]]:
+        if not ranges: return []
+        result, last_range = [], []
+
+        for rng in sorted(ranges):
+            if not last_range:
+                last_range = rng
+                continue
+
+            if rng[0] <= last_range[1]:
+                last_range = (last_range[0], max(rng[1], last_range[1]))
+
+            else:
+                result.append(last_range)
+                last_range = rng
+
+        else: result.append(last_range)
+        return result
+
+
+if __name__ == '__main__':
+    solve = Solution()
+    print(solve.main([[1, 3], [100, 200], [2, 4]]))
