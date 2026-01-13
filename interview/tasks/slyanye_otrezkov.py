76347bfe49fe95b37417f47ff57f4dba11eedc5a
FlexonaFFt <flexonafft@bk.ru>
2026-01-13 22:33:15 +0300

    interview slyanye otrezkov --repeat

--- interview/tasks/slyanye_otrezkov.py
+++ interview/tasks/slyanye_otrezkov.py
@@ -25,7 +25,25 @@ class Solution:
         else: result.append(last_range)
         return result
 
+    def myprobe(self, ranges: list[list[int]]):
+        if not ranges: return []
+        output, current = [], None
+
+        for range in sorted(ranges):
+            if not current:
+                current = range
+                continue
+
+            if range[0] <= current[1]:
+                current = (current[0], max(range[1], current[1]))
+            else:
+                output.append(current)
+                current = range
+
+        else: output.append(current)
+        return output
 
 if __name__ == '__main__':
     solve = Solution()
     print(solve.main([[1, 3], [100, 200], [2, 4]]))
+    print(solve.myprobe([[1, 3], [100, 200], [2, 4]]))
