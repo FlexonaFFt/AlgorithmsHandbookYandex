682d8b0abdb4d873274e06596d0af71a0288e10b
FlexonaFFt <flexonafft@bk.ru>
2026-01-24 16:40:53 +0300

    train2 fifth OK

---
+++ interview/mytrain/trains/train2/fifth.py
@@ -0,0 +1,14 @@
+class Solution:
+    def main(self, nums: list[int]) -> list[int]:
+        if not nums: return []
+
+        slow = 0
+
+        for fast in range(1, len(nums)):
+            if nums[fast] == nums[fast - 1]:
+                nums[slow] = nums[fast]
+                slow += 1
+        return nums[:slow]
+
+
+print(Solution().main([0,0,1,1,2,2,3]))
